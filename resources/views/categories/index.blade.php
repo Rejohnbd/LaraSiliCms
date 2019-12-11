f71b49689a9daf585afb6cc6ca77a455faefb97a
Rejohnbd <rejohn.cse@gmail.com>
2019-12-11 20:10:08 +0600

    Delete Category using Delete method

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -13,11 +13,51 @@
                 @foreach($categories as $category)
                 <tr>
                     <td>{{ $category->name }}</td>
-                    <td><a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                    <td>
+                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
+                        <button class="btn btn-danger btn-sm" onclick="handelDelete({{ $category->id }})">Delete</button>
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
+
+        <form action="" method="POST" id="deleteCategoryForm">
+            @csrf
+            @method('DELETE')
+            <div class="modal fade" id="cmsDeleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteModal">Delete Category</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="text-center text-bold"> 
+                            Are you Sure? You want to delete this Category. 
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go Back</button>
+                        <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+
     </div>
 </div>
+@endsection
+
+@section('scripts')
+    <script>
+        function handelDelete(id){
+            var form = document.getElementById('deleteCategoryForm');
+            form.action = '/categories/' + id;
+            $('#cmsDeleteModal').modal('show');
+        }
+    </script>
 @endsection
