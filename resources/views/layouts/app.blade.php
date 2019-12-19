6f6d7da1b0ac5b16891102df13ac7eb346bc153e
Rejohnbd <rejohn.cse@gmail.com>
2019-12-20 01:42:48 +0600

    Create User Controller, Modify User Table, Middleware and User Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,8 +104,11 @@
                     <div class="row">
                         <div class="col-md-4">
                             <ul class="list-group">
-                                <li class="list-group-item"><a href="{{ route('posts.index') }}">Posts</a></li>
+                                @if(auth()->user()->isAdmin())
+                                    <li class="list-group-item"><a href="{{ route('users.index') }}">Users</a></li>
+                                @endif
                                 <li class="list-group-item"><a href="{{ route('tags.index') }}">Tags</a></li>
+                                <li class="list-group-item"><a href="{{ route('posts.index') }}">Posts</a></li>
                                 <li class="list-group-item"><a href="{{ route('categories.index') }}">Categories</a></li>
                             </ul>
                             <ul class="list-group mt-5">
